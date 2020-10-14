c74d5d7729853788b780e3cdc73a2b6c5a0f5cd3
TharindaNimnajith <tharindarajapakshe@gmail.com>
2020-10-14 14:10:59 +0530

    code refactoring

--- app/Pages/Inventory/inventory-list.tsx
+++ app/Pages/Inventory/inventory-list.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
 import { proxy } from '../../conf';
-import { setEditingInventory, setEditingInventoryId, setEditInventory, setExistingInventory } from './inventory-slice';
-import { Button, Modal, Spinner, Table } from 'react-bootstrap';
-import { FaEdit, FaTrashAlt } from 'react-icons/all';
+// import { setEditingInventory, setEditingInventoryId, setEditInventory, setExistingInventory } from './inventory-slice';
+import { Table } from 'react-bootstrap';
 
 let errors_: string = '';
 
 const InventoryList: React.FC = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   // let inventoryList = useSelector(
   //   (state: {
@@ -16,9 +14,9 @@ const InventoryList: React.FC = () => {
   //   }) => state.inventory.inventory
   // );
 
-  const [loading, setLoading] = useState<boolean>(false);
-  const [show, setShow] = useState<boolean>(false);
-  const [deleteId, setDeleteId] = useState<string>('');
+  // const [loading, setLoading] = useState<boolean>(false);
+  // const [show, setShow] = useState<boolean>(false);
+  // const [deleteId, setDeleteId] = useState<string>('');
   const [inventories, setInventoryList] = useState<any>([]);
   // const [inventory, setInventory] = useState<{
   //   itemId: string,
@@ -40,7 +38,7 @@ const InventoryList: React.FC = () => {
 
   const getInventory = async () => {
     try {
-      setLoading(true);
+      // setLoading(true);
       const response = await fetch(`${proxy}/inventory/getInventories`, {
         method: 'GET',
         headers: {
@@ -50,10 +48,10 @@ const InventoryList: React.FC = () => {
       const responseData = await response.json();
       setInventoryList(responseData);
       // await dispatch(setInventory(responseData));
-      setLoading(false);
+      // setLoading(false);
     } catch (errors) {
       errors_ = errors;
-      setLoading(false);
+      // setLoading(false);
       console.log(errors);
     }
   };
@@ -81,71 +79,71 @@ const InventoryList: React.FC = () => {
   //   setLoading(false)
   // }
 
-  const handleClose = () => {
-    setLoading(true);
-    setShow(false);
-    setLoading(false);
-  };
-
-  const handleDelete = () => {
-    setLoading(true);
-    deleteInventory(deleteId).then(() => setShow(false));
-    setLoading(false);
-  };
-
-  const handleShow = (id: string) => {
-    setLoading(true);
-    setShow(true);
-    setDeleteId(id);
-    setLoading(false);
-  };
-
-  const editInventory = async (id: string) => {
-    setLoading(true);
-    try {
-      const response = await fetch(`${proxy}/inventory/getInventories/` + id, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      const responseData = await response.json();
-      await dispatch(setExistingInventory(false));
-      await dispatch(setEditingInventoryId(id));
-      await dispatch(setEditingInventory(responseData));
-      await dispatch(setEditInventory(true));
-      setLoading(false);
-    } catch (errors) {
-      errors_ = errors;
-      setLoading(false);
-      console.log(errors);
-    }
-  };
-
-  const deleteInventory = async (id: string) => {
-    setLoading(true);
-    try {
-      const response = await fetch(`${proxy}/inventory/deleteInventories`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          id
-        })
-      });
-      await response.json();
-      // inventoryList = inventoryList.filter((inventory: any) => inventory._id !== id);
-      // await dispatch(setInventory(inventoryList));
-      await dispatch(setEditInventory(false));
-      await dispatch(setExistingInventory(false));
-      setLoading(false);
-    } catch (errors) {
-      errors_ = errors;
-      setLoading(false);
-      console.log(errors);
-    }
-  };
+  // const handleClose = () => {
+  //   setLoading(true);
+  //   setShow(false);
+  //   setLoading(false);
+  // };
+  //
+  // const handleDelete = () => {
+  //   setLoading(true);
+  //   deleteInventory(deleteId).then(() => setShow(false));
+  //   setLoading(false);
+  // };
+  //
+  // const handleShow = (id: string) => {
+  //   setLoading(true);
+  //   setShow(true);
+  //   setDeleteId(id);
+  //   setLoading(false);
+  // };
+  //
+  // const editInventory = async (id: string) => {
+  //   setLoading(true);
+  //   try {
+  //     const response = await fetch(`${proxy}/inventory/getInventories/` + id, {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     });
+  //     const responseData = await response.json();
+  //     await dispatch(setExistingInventory(false));
+  //     await dispatch(setEditingInventoryId(id));
+  //     await dispatch(setEditingInventory(responseData));
+  //     await dispatch(setEditInventory(true));
+  //     setLoading(false);
+  //   } catch (errors) {
+  //     errors_ = errors;
+  //     setLoading(false);
+  //     console.log(errors);
+  //   }
+  // };
+  //
+  // const deleteInventory = async (id: string) => {
+  //   setLoading(true);
+  //   try {
+  //     const response = await fetch(`${proxy}/inventory/deleteInventories`, {
+  //       method: 'DELETE',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({
+  //         id
+  //       })
+  //     });
+  //     await response.json();
+  //     // inventoryList = inventoryList.filter((inventory: any) => inventory._id !== id);
+  //     // await dispatch(setInventory(inventoryList));
+  //     await dispatch(setEditInventory(false));
+  //     await dispatch(setExistingInventory(false));
+  //     setLoading(false);
+  //   } catch (errors) {
+  //     errors_ = errors;
+  //     setLoading(false);
+  //     console.log(errors);
+  //   }
+  // };
 
   return (
     <div>
@@ -203,39 +201,39 @@ const InventoryList: React.FC = () => {
       {/*  marginTop: '4%'*/}
       {/*}}>*/}
       <div>
-        <Modal show={show}
-               onHide={handleClose}
-               deleteId={deleteId}>
-          <Modal.Header closeButton>
-            <Modal.Title>Delete Inventory</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
-          <Modal.Footer>
-            <Button variant='success'
-                    onClick={handleClose}
-                    style={{
-                      textTransform: 'uppercase'
-                    }}>
-              Close
-            </Button>
-            <Button variant='danger'
-                    onClick={handleDelete}
-                    style={{
-                      textTransform: 'uppercase'
-                    }}>
-              Delete
-            </Button>
-          </Modal.Footer>
-          {
-            loading && (
-              <Spinner animation='border'
-                       style={{
-                         textAlign: 'center',
-                         marginLeft: '50%'
-                       }} />
-            )
-          }
-        </Modal>
+        {/*<Modal show={show}*/}
+        {/*       onHide={handleClose}*/}
+        {/*       deleteId={deleteId}>*/}
+        {/*  <Modal.Header closeButton>*/}
+        {/*    <Modal.Title>Delete Inventory</Modal.Title>*/}
+        {/*  </Modal.Header>*/}
+        {/*  <Modal.Body>Are you sure you want to delete this item?</Modal.Body>*/}
+        {/*  <Modal.Footer>*/}
+        {/*    <Button variant='success'*/}
+        {/*            onClick={handleClose}*/}
+        {/*            style={{*/}
+        {/*              textTransform: 'uppercase'*/}
+        {/*            }}>*/}
+        {/*      Close*/}
+        {/*    </Button>*/}
+        {/*    <Button variant='danger'*/}
+        {/*            onClick={handleDelete}*/}
+        {/*            style={{*/}
+        {/*              textTransform: 'uppercase'*/}
+        {/*            }}>*/}
+        {/*      Delete*/}
+        {/*    </Button>*/}
+        {/*  </Modal.Footer>*/}
+        {/*  {*/}
+        {/*    loading && (*/}
+        {/*      <Spinner animation='border'*/}
+        {/*               style={{*/}
+        {/*                 textAlign: 'center',*/}
+        {/*                 marginLeft: '50%'*/}
+        {/*               }} />*/}
+        {/*    )*/}
+        {/*  }*/}
+        {/*</Modal>*/}
         <Table responsive
                striped
                bordered
@@ -308,22 +306,33 @@ const InventoryList: React.FC = () => {
           }}>
             Status
           </th>
+          {/*<th style={{*/}
+          {/*  borderBottom: 'solid darkblue 1px',*/}
+          {/*  borderTop: 'solid darkblue 1px',*/}
+          {/*  textAlign: 'center',*/}
+          {/*  fontSize: 'large',*/}
+          {/*  fontWeight: 'lighter',*/}
+          {/*  color: 'white'*/}
+          {/*}}>*/}
+          {/*  Is Restricted*/}
+          {/*</th>*/}
+          {/*<th colSpan={2}*/}
+          {/*    style={{*/}
+          {/*      borderBottom: 'solid darkblue 1px',*/}
+          {/*      borderRight: 'solid darkblue 1px',*/}
+          {/*      borderTop: 'solid darkblue 1px'*/}
+          {/*    }} />*/}
           <th style={{
-            borderBottom: 'solid darkblue 1px',
-            borderTop: 'solid darkblue 1px',
             textAlign: 'center',
             fontSize: 'large',
             fontWeight: 'lighter',
-            color: 'white'
+            color: 'white',
+            borderBottom: 'solid darkblue 1px',
+            borderRight: 'solid darkblue 1px',
+            borderTop: 'solid darkblue 1px'
           }}>
             Is Restricted
           </th>
-          <th colSpan={2}
-              style={{
-                borderBottom: 'solid darkblue 1px',
-                borderRight: 'solid darkblue 1px',
-                borderTop: 'solid darkblue 1px'
-              }} />
           </thead>
           <tbody>
           {
@@ -387,30 +396,30 @@ const InventoryList: React.FC = () => {
                       </td>
                     )
                   }
-                  <td style={{
-                    textAlign: 'center'
-                  }}>
-                    <button onClick={() => editInventory(inventory._id)}
-                            style={{
-                              color: 'darkgreen',
-                              backgroundColor: 'transparent',
-                              border: 'none'
-                            }}>
-                      <FaEdit size={20} />
-                    </button>
-                  </td>
-                  <td style={{
-                    textAlign: 'center'
-                  }}>
-                    <button onClick={() => handleShow(inventory._id)}
-                            style={{
-                              color: 'indianred',
-                              backgroundColor: 'transparent',
-                              border: 'none'
-                            }}>
-                      <FaTrashAlt size={20} />
-                    </button>
-                  </td>
+                  {/*<td style={{*/}
+                  {/*  textAlign: 'center'*/}
+                  {/*}}>*/}
+                  {/*  <button onClick={() => editInventory(inventory._id)}*/}
+                  {/*          style={{*/}
+                  {/*            color: 'darkgreen',*/}
+                  {/*            backgroundColor: 'transparent',*/}
+                  {/*            border: 'none'*/}
+                  {/*          }}>*/}
+                  {/*    <FaEdit size={20} />*/}
+                  {/*  </button>*/}
+                  {/*</td>*/}
+                  {/*<td style={{*/}
+                  {/*  textAlign: 'center'*/}
+                  {/*}}>*/}
+                  {/*  <button onClick={() => handleShow(inventory._id)}*/}
+                  {/*          style={{*/}
+                  {/*            color: 'indianred',*/}
+                  {/*            backgroundColor: 'transparent',*/}
+                  {/*            border: 'none'*/}
+                  {/*          }}>*/}
+                  {/*    <FaTrashAlt size={20} />*/}
+                  {/*  </button>*/}
+                  {/*</td>*/}
                 </tr>
               );
             })
